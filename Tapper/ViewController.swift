29f160d98ab61813a4ad0626d2c3771c4c6eccc1
SoengSaravit <soengsaravit@gmail.com>
2017-08-15 22:37:30 +0700

    New Commit

--- Tapper/ViewController.swift
+++ Tapper/ViewController.swift
@@ -9,17 +9,52 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var tapperImage: UIImageView!
+    @IBOutlet weak var playButton: UIButton!
 
+    @IBOutlet weak var inputTextField: UITextField!
+    
+    @IBOutlet weak var tabLabel: UILabel!
+    
+    @IBOutlet weak var tabButton: UIButton!
+    
+    var maxTab:Int?
+    var currentTab:Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func tabButtonClick(_ sender: Any) {
+        currentTab += 1
+        if currentTab <= maxTab! {
+            tabLabel.text = "\(currentTab) Tabs"
+        }else{
+            isGameStart(b: false)
+            currentTab = 0
+            inputTextField.text = ""
+            tabLabel.text = "0 Tab"
+        }
     }
-
-
+    @IBAction func playButtonClick(_ sender: Any) {
+        if inputTextField.text != nil && inputTextField.text != "" {
+             maxTab = Int(inputTextField.text!)
+            isGameStart(b: true)
+        }
+       
+    }
+    
+    func isGameStart(b:Bool){
+        
+        tabButton.isHidden = !b
+        tabLabel.isHidden = !b
+        
+        tapperImage.isHidden = b
+        inputTextField.isHidden = b
+        playButton.isHidden = b
+        
+    }
 }
 
